940f0e3bd80e9327f39a89bc4837997d208a2106
alfonso domenici <alfonso.domenici@gmail.com>
2024-06-20 09:35:56 +0200

    token manager

--- resources/js/login.js
+++ resources/js/login.js
@@ -1,3 +1,5 @@
+import { storeToken } from './tokenManager.js'
+
 document.querySelector('#btnAccedi')
     .addEventListener('click', onLogin);
 
@@ -5,5 +7,21 @@ function onLogin(e) {
     e.preventDefault();
     const email = document.querySelector('#email').value;
     const pwd = document.querySelector('#pwd').value;
-    console.log(email,pwd);
+    const credential = { email, pwd }
+    fetch('http://127.0.0.1:5000/api/auths/', {
+        method: 'POST',
+        mode: 'cors',
+        cache: "no-cache",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credential)
+    }).then(response => {
+        console.log(response);
+        return response.json();
+    }).then(data => {
+        console.log(data);
+        storeToken(data.token);
+        window.location='./auth/prenotazioni.html'
+    })
 }
